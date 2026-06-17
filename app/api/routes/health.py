7cdb0df8dc14cdac1c5e0d375d851c36c34417c7
YeisonMC <yeisonmacalupu01@gmail.com>
2026-06-17 09:08:53 -0500

    methods add head

--- app/api/routes/health.py
+++ app/api/routes/health.py
@@ -3,7 +3,14 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/health")
+# @router.get("/health")
+# def health_check():
+#     return {
+#         "status": "ok",
+#         "message": "Backend del recomendador musical funcionando correctamente."
+#     }
+
+@router.api_route("/health", methods=["GET", "HEAD"])
 def health_check():
     return {
         "status": "ok",
